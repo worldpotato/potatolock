02dcea831aa46d3c99657dcc792fabb458adbaba
Felix Strobel <mail@felixstrobel.de>
2020-05-25 21:56:18 +0200

    apply color

--- config.def.h
+++ config.def.h
@@ -1,11 +1,11 @@
 /* user and group to drop privileges to */
-static const char *user  = "nobody";
-static const char *group = "nogroup";
+static const char *user  = "worldpotato";
+static const char *group = "worldpotato";
 
 static const char *colorname[NUMCOLS] = {
-	[INIT] =   "black",     /* after initialization */
-	[INPUT] =  "#005577",   /* during input */
-	[FAILED] = "#CC3333",   /* wrong password */
+	[INIT] =   "#022222",     /* after initialization */
+	[INPUT] =  "#033333",   /* during input */
+	[FAILED] = "#aa0000",   /* wrong password */
 };
 
 /* treat a cleared input like a wrong password (color) */
